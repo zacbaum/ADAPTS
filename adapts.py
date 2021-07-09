5597e50003ffbaab2b2181160c5098625d34a28f
zacbaum <zac_baum@hotmail.com>
2021-07-09 11:08:06 +0100

    Speed-up GRAD_CAM by reducing repeated processes, only show covid score for good images, enlarge text, show scores in colour

--- adapts.py
+++ adapts.py
@@ -8,18 +8,13 @@ import sys
 from tensorflow.keras.models import load_model
 
 QA_THRESH = 0.5
+COVID_THRESH = 0.5
 
-def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
-    # First, we create a model that maps the input image to the activations
-    # of the last conv layer as well as the output predictions
-    grad_model = tf.keras.models.Model(
-        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
-    )
-
-    # Then, we compute the gradient of the top predicted class for our input image
+def make_gradcam_heatmap(img_array, grad_model, pred_index=None):
+    # We compute the gradient of the top predicted class for our input image
     # with respect to the activations of the last conv layer
     with tf.GradientTape() as tape:
-        last_conv_layer_output, preds = grad_model(img_array)
+        last_conv_layer_output, preds = grad_model(img_array, training=False)
         if pred_index is None:
             pred_index = tf.argmax(preds[0])
         class_channel = preds[:, pred_index]
@@ -43,14 +38,14 @@ def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None
     heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
     return heatmap.numpy(), preds
 
-def get_added_gradcam(img, heatmap, alpha=0.2):
+def get_added_gradcam(img, heatmap, alpha=0.1):
     
     # Rescale heatmap to a range 0-255
     heatmap = np.uint8(255 * heatmap)
 
     # Use colormap to the CAM
     heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]), interpolation=cv2.INTER_LANCZOS4)
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
     
     # Add the images together
     superimposed_img = cv2.addWeighted(img[:, :, :3], 1.0, heatmap, alpha, 0)
@@ -71,19 +66,25 @@ rescale_factor = 2
 
 window = "ADAPTS"
 cv2.namedWindow(window)
-cv2.moveWindow(window, left + width + 10, top - 30)
+cv2.moveWindow(window, left + width + 500, top - 200)
 
 with mss.mss() as sct:
 
     qa_model = load_model("models/quality-cls-50to1-effnetb0.h5", compile=False)
-    qa_model.call = tf.function(qa_model.call, experimental_relax_shapes=True)
+    qa_model.call = tf.function(qa_model.call)
 
     d_model = load_model("models/fold1-effnetb0.h5", compile=False)
-    d_model.call = tf.function(d_model.call, experimental_relax_shapes=True)
+    d_model.call = tf.function(d_model.call)
 
+    if not use_unet:
+	    # Create a model that maps the input image to the activations
+	    # of the last conv layer as well as the output predictions
+	    d_model_grads = tf.keras.models.Model(
+	        [d_model.inputs], [d_model.get_layer('top_conv').output, d_model.output] # top_conv is specific to effnetb0
+	    )
     if use_unet:
         unet_model = load_model("models/unet.h5", compile=False)
-        unet_model.call = tf.function(unet_model.call, experimental_relax_shapes=True)
+        unet_model.call = tf.function(unet_model.call)
 
     while True:
         start_time = time.time()
@@ -95,77 +96,91 @@ with mss.mss() as sct:
         img_gr = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
 
         # Rescale to proper size
-        img_scaled = cv2.resize(img_gr, (180, 270)) # Update this to be the size of network inputs
+        img_scaled = cv2.resize(
+        	img_gr,
+        	(qa_model.input_shape[2], qa_model.input_shape[1]), # cv2 uses flipped h,w from tf
+        	interpolation=cv2.INTER_NEAREST,
+        )
         if use_unet:
-            img_scaled_seg = cv2.resize(img_gr, (256, 384)) # Update this to be the size of network inputs
-
-        # Batch dim, channels dim
-        img_processed = np.expand_dims(np.expand_dims(img_scaled, axis=0), axis=-1)
+            img_scaled_seg = cv2.resize(
+            	img_gr,
+            	(unet_model.input_shape[2], unet_model.input_shape[1]), # cv2 uses flipped h,w from tf
+            	interpolation=cv2.INTER_NEAREST,
+            )
+        # Add batch dim, channels dim
+        img_processed = img_scaled[np.newaxis, ..., np.newaxis]
         if use_unet:
-            img_processed_seg = np.expand_dims(np.expand_dims(img_scaled_seg, axis=0), axis=-1)
-            img_normalized_seg = (img_processed_seg - np.min(img_processed_seg))/np.ptp(img_processed_seg)
+            img_processed_seg = img_scaled_seg[np.newaxis, ..., np.newaxis]
+            img_normalized_seg = (img_processed_seg - np.min(img_processed_seg)) / np.ptp(img_processed_seg)
                 
-        qa_pred = qa_model(img_processed.astype(np.float32), training=False)[0]
-        qa_str = "QA: {:.1f}".format(np.squeeze(qa_pred))
+        qa_pred = qa_model(img_processed, training=False)[0]
+        qa_str = "QA: {:.2f}".format(np.squeeze(qa_pred))
 
         if qa_pred[0] >= QA_THRESH:
-            
             if use_unet:
-                unet_pred = unet_model(img_normalized_seg.astype(np.float32), training=False)[0]
-                unet_pred_rescaled = cv2.resize(np.squeeze(unet_pred), (width, height))
+                unet_pred = unet_model(img_normalized_seg, training=False)[0]
+                unet_pred_rescaled = cv2.resize(
+                	np.squeeze(unet_pred),
+                	(width, height),
+                	interpolation=cv2.INTER_NEAREST,
+                )
                 unet_pred_denormed = (255 * (unet_pred_rescaled - np.min(unet_pred_rescaled)) / np.ptp(unet_pred_rescaled)).astype(np.uint8)        
-                img = cv2.addWeighted(np.expand_dims(img_gr, axis=-1), 1.0, np.expand_dims(unet_pred_denormed, axis=-1), 0.75, 0)
-        
+                img = cv2.addWeighted(img_gr[..., np.newaxis], 1.0, unet_pred_denormed[..., np.newaxis], 0.25, 0)
+                img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGRA)
                 # If we use the unet, we also need to get the diagnosistic likelihood
-                d_pred = d_model(img_processed.astype(np.float32), training=False)[0]
-
+                d_pred = d_model(img_processed, training=False)[0]
             if not use_unet:
-                # If we don't use the unet, we get the diagnositic likelihood from the gradcam outputs
-                heatmap, d_pred = make_gradcam_heatmap(img_processed, d_model, 'top_conv')
+                # If we don't use the unet, we get the diagnositic likelihood from the gradcam model
+                heatmap, d_pred = make_gradcam_heatmap(img_processed, d_model_grads)
                 img = get_added_gradcam(img, heatmap)
-
-            covid_str = "COVID: {:.1f}".format(np.squeeze(d_pred))
-        
+            covid_str = "COVID: {:.2f}".format(np.squeeze(d_pred))
         else:
-            covid_str = "COVID: N/A"  
+            covid_str = ""  
 
         if rescale_factor != 1:
-            img = cv2.resize(img, (img.shape[1] * rescale_factor, img.shape[0] * rescale_factor))
+            img = cv2.resize(
+            	img,
+            	(img.shape[1] * rescale_factor, img.shape[0] * rescale_factor),
+            	interpolation=cv2.INTER_NEAREST,
+            )
         
         font = cv2.FONT_HERSHEY_SIMPLEX
-        scale = 0.5
-        text_color = (255, 255, 255)
-        px_width = 1
+        scale = 1.0
+        text_color_white = (255, 255, 255)
+        text_color_green = (0, 255, 127) # BGR
+        text_color_red = (0, 8, 255) # BGR
+        px_width = 2
 
         cv2.putText(
             img, 
             qa_str,
-            (5, height * rescale_factor - 25),
+            (5, height * rescale_factor - 10),
             font,
             scale,
-            text_color,
+            text_color_green if np.squeeze(qa_pred) >= QA_THRESH else text_color_red,
             px_width,
         )
 
-        cv2.putText(
-            img, 
-            covid_str,
-            (5, height * rescale_factor - 5),
-            font,
-            scale,
-            text_color,
-            px_width,
-        )
+        if covid_str is not "":
+	        cv2.putText(
+	            img, 
+	            covid_str,
+	            (5, height * rescale_factor - 40),
+	            font,
+	            scale,
+	            text_color_red if np.squeeze(d_pred) >= COVID_THRESH else text_color_green,
+	            px_width,
+	        )
 
         if show_fps: 
             fps_str = "FPS: {:.1f}".format(1 / (time.time() - start_time))
             cv2.putText(
                 img, 
                 fps_str,
-                (width * rescale_factor - 75, height * rescale_factor - 5),
+                (width * rescale_factor - 170, height * rescale_factor - 10),
                 font,
                 scale,
-                text_color,
+                text_color_white,
                 px_width,
             )
 
